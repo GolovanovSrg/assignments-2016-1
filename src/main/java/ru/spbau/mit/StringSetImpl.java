fe9130801d90ff24ec82b3c13fa38f8fba480eb9
Golovanov <Sergey_XG@mail.ru>
2016-02-21 17:23:34 +0300

    hw1

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -3,5 +3,145 @@ package ru.spbau.mit;
 /**
  * Created by golovanov on 21.02.16.
  */
-public class StringSetImpl {
+public class StringSetImpl implements StringSet {
+
+    private StringSetNode root;
+
+    private class StringSetNode {
+        StringSetNode[] children;
+        int sizeSubTree;
+        boolean isUsed;
+
+        public StringSetNode() {
+            children =  new StringSetNode[52];
+            sizeSubTree = 0;
+            isUsed = false;
+        }
+
+        private int index(char ch) {
+            final byte lowerOffset = 71; //ASCII
+            final byte upperOffset = 65; //ASCII
+            final byte aNo = 97; //ASCII
+
+            if (ch >= aNo) {
+                return ch - lowerOffset;
+            } else {
+                return ch - upperOffset;
+            }
+        }
+
+        public StringSetNode getNextNode(char ch) {
+            int idxNextNode = index(ch);
+            StringSetNode nextNode = children[idxNextNode];
+
+            if (nextNode == null) {
+                nextNode = children[idxNextNode] = new StringSetNode();
+            }
+
+            return nextNode;
+        }
+
+        public StringSetNode findNextNode(char ch) {
+            int idxNextNode = index(ch);
+            StringSetNode nextNode = children[idxNextNode];
+
+            if (nextNode == null) {
+                return null;
+            }
+
+            return nextNode;
+        }
+    }
+
+    public StringSetImpl() {
+        root = new StringSetNode();
+    }
+
+    public StringSetImpl(String element) {
+        this();
+        add(element);
+    }
+
+    @Override
+    public boolean add(String element) {
+        StringSetNode curNode = root;
+
+        for (char ch : element.toCharArray()) {
+            curNode = curNode.getNextNode(ch);
+        }
+
+        if (!curNode.isUsed) {
+            curNode.isUsed = true;
+
+            curNode = root;
+            for (char ch : element.toCharArray()) {
+                curNode.sizeSubTree += 1;
+                curNode = curNode.getNextNode(ch);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean contains(String element) {
+        StringSetNode curNode = root;
+
+        for (char ch : element.toCharArray()) {
+            curNode = curNode.findNextNode(ch);
+
+            if (curNode == null) {
+                return false;
+            }
+        }
+
+        if (!curNode.isUsed) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean remove(String element) {
+        if (contains(element)) {
+            StringSetNode curNode = root;
+
+            for (char ch : element.toCharArray()) {
+                int idxNextNode = curNode.index(ch);
+
+                if (curNode.sizeSubTree == 1) {
+                    curNode.children[idxNextNode] = null;
+                    curNode.sizeSubTree = 0;
+                    return true;
+                }
+
+                curNode = curNode.children[idxNextNode];
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return root.sizeSubTree;
+    }
+
+    @Override
+    public int howManyStartsWithPrefix(String prefix) {
+        StringSetNode curNode = root;
+
+        for (char ch : prefix.toCharArray()) {
+            curNode = curNode.findNextNode(ch);
+
+            if (curNode == null) {
+                return 0;
+            }
+        }
+
+        return curNode.sizeSubTree == 0 ? 1 : curNode.sizeSubTree;
+    }
 }
